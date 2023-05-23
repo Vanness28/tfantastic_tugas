14b0330a9b233ba9f9b7a448ad4230c8f3c8382c
ig-tangka <ignatiustangka@gmail.com>
2023-05-23 19:29:07 +0800

    sixth commit

---
+++ app/src/main/java/com/example/tfantastictugas/DetailHero.kt
@@ -0,0 +1,11 @@
+package com.example.tfantastictugas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailHero : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_hero)
+    }
+}
